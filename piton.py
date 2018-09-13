3ae73836bd35af29ac384a1f93de5c20dfa4255e
Destroidom <43251498+Destroidom@users.noreply.github.com>
2018-09-13 17:26:43 -0300

    Create piton.py

---
+++ piton.py
@@ -0,0 +1,20 @@
+while True:
+    answer = input("Qual a resposta para a vida, o universo e tudo mais")
+    if(answer == "42"):
+        break
+    elif(answer == "11"):
+        print("nooo")
+for count in range(0, 10):
+    print("contagem regressiva: " + str(count))
+
+# ESSE É UM COMENTARIO
+texto = "Eu sou eu"
+decimal = 3.14
+inteiro = 10
+booleanas = True
+
+def soma(x, y):
+    resultado = x + y
+    print("O resultado é igual a: " + str(resultado))
+
+soma(4, 5)
